6f54500b64ab3a4474302d9ba98da1392d968bf7
sridhar-sp <sridharthechosenone@gmail.com>
2022-01-03 16:35:06 +0530

    Add main content

--- app/src/main/java/com/gandiva/androidsplashscreen/MainActivity.kt
+++ app/src/main/java/com/gandiva/androidsplashscreen/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gandiva.androidsplashscreen.ui.theme.AndroidSplashScreenTheme
@@ -26,7 +32,7 @@ class MainActivity : ComponentActivity() {
             AndroidSplashScreenTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
@@ -34,14 +40,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Android splash screen demo",
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AndroidSplashScreenTheme {
-        Greeting("Android")
+        MainContent()
     }
 }
